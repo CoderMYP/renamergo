7170678b4e4f2b59b5edd854400e3c9d7f501457
ae010108 <117028164+ae010108@users.noreply.github.com>
2022-12-02 00:51:53 +0500

    Update refer.py

--- plugins/refer.py
+++ plugins/refer.py
@@ -1,8 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
-@Client.on_message(filters.private & filters.command(["refer"]))
+@Client.on_message(filters.private & filters.command(["referal"]))
 async def refer(client,message):
     reply_markup = InlineKeyboardMarkup(
-       		[ [ InlineKeyboardButton("Share Your Link" ,url=f"https://t.me/share/url?url=https://t.me/rename_urbot?start={message.from_user.id}") ]   ])
-    await message.reply_text(f"Refer And Earn Get 100MB Upload Limit\nPer Refer 100MB\n Your Link :- https://t.me/rename_urbot?start={message.from_user.id} ",reply_to_message_id = message.id,reply_markup=reply_markup,)
-    
+       		[ [ InlineKeyboardButton("Shaxsiy havolani tarqatish" ,url=f"https://t.me/share/url?url=https://t.me/azik_renamerbot?start={message.from_user.id}") ]   ])
+    await message.reply_text(f"Do'stni chaqiring va har bir do'stingiz uchun 100Mb qo'shimcha joy olasiz!\nSizning havolangiz: https://t.me/azik_renamerbot?start={message.from_user.id}\n\n\n<b>Do'stingiz botga start bosib kanalga a'zo bo'lishi kerak kanaldan chiqsa yoki botni bloklasa sizdan joy olib tashlanadi!!!</b>",reply_to_message_id = message.id,reply_markup=reply_markup,)
