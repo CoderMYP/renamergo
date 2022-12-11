cc558ec9d26e0e9c00ed7029e7c28e809a32b147
Khurban05 <119966317+Khurban05@users.noreply.github.com>
2022-12-11 11:35:48 +0500

    Update refer.py

--- plugins/refer.py
+++ plugins/refer.py
@@ -3,5 +3,5 @@ from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceRep
 @Client.on_message(filters.private & filters.command(["referal"]))
 async def refer(client,message):
     reply_markup = InlineKeyboardMarkup(
-       		[ [ InlineKeyboardButton("Shaxsiy havolani tarqatish" ,url=f"https://t.me/share/url?url=https://t.me/azik_renamerbot?start={message.from_user.id}") ]   ])
+       		[ [ InlineKeyboardButton("Shaxsiy havolani tarqatish" ,url=f"https://t.me/share/url?url=https://t.me/Renamere_Bot?start={message.from_user.id}") ]   ])
     await message.reply_text(f"Do'stni chaqiring va har bir do'stingiz uchun 100Mb qo'shimcha joy olasiz!\nSizning havolangiz: https://t.me/azik_renamerbot?start={message.from_user.id}\n\n\n<b>Do'stingiz botga start bosib kanalga a'zo bo'lishi kerak kanaldan chiqsa yoki botni bloklasa sizdan joy olib tashlanadi!!!</b>",reply_to_message_id = message.id,reply_markup=reply_markup,)
