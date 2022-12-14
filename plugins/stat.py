07eb2ae7098a227c9a053866d9d86e70f4bfd3fe
Khurban05 <119966317+Khurban05@users.noreply.github.com>
2022-12-14 13:28:57 +0500

    Create stat.py

---
+++ plugins/stat.py
@@ -0,0 +1,16 @@
+import os
+import asyncio
+from pyrogram import Client ,filters
+from helper.database import getid ,delete
+import time
+ADMIN = int(os.environ.get("ADMIN", 795726700))
+ 
+
+@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["stat"]))
+async def stat(bot, message):
+   ms = await message.reply_text("Azolar sanalmoqda...........")
+   ids = getid()
+   tot = len(ids)
+   success = 0 
+   failed = 0 
+   await ms.edit(f"Bot foydalanuvchilari {tot} ta")
